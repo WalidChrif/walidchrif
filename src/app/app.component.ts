76cb8155054acb5f448cf370271664ceeed42d6b
WalidChrif <walid.chrif111@gmail.com>
2024-02-11 20:29:21 +0100

    first commit by me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { FeaturesComponent } from './features/features.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
+import { StartedComponent } from './started/started.component';
+import { ConsultComponent } from './consult/consult.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroductionComponent, AboutComponent, ResumeComponent,
+     FeaturesComponent, SkillsComponent, ProjectsComponent, BlogComponent, StartedComponent,
+     ConsultComponent, FooterComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
